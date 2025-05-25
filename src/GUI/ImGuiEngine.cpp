34bae1617d51baa78ad9c091ac2221118f9e7f91
Seyyit Genc <seyyitgenc08@gmail.com>
2025-05-25 11:52:05 +0300

    fixed build erros and integrated implot

--- src/GUI/ImGuiEngine.cpp
+++ src/GUI/ImGuiEngine.cpp
@@ -1,9 +1,9 @@
 #include "ImGuiEngine.hpp"
 #include "ImGuiPanel.hpp"
 
-#include <backends/imgui_impl_glfw.h>
-#include <backends/imgui_impl_opengl3.h>
 #include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 #include <imgui_internal.h>
 
 #define GL_SILENCE_DEPRECATION
